c232058e4cf8b97802147660a36ac972863f3e1e
tanhengyeow <E0032242@u.nus.edu>
2018-10-17 02:13:42 +0800

    Added new REST endpoint to share presentation

--- src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/PresentationController.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/PresentationController.java
@@ -1,8 +1,10 @@
 package sg.edu.nus.comp.cs3219.viz.ui.controller.api;
 
+import sg.edu.nus.comp.cs3219.viz.common.datatransfer.AccessControlMapping;
 import sg.edu.nus.comp.cs3219.viz.common.datatransfer.AccessLevel;
 import sg.edu.nus.comp.cs3219.viz.common.datatransfer.UserInfo;
 import sg.edu.nus.comp.cs3219.viz.common.entity.Presentation;
+import sg.edu.nus.comp.cs3219.viz.common.entity.PresentationAccessControl;
 import sg.edu.nus.comp.cs3219.viz.common.exception.PresentationNotFoundException;
 import sg.edu.nus.comp.cs3219.viz.logic.GateKeeper;
 import sg.edu.nus.comp.cs3219.viz.logic.PresentationLogic;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sg.edu.nus.comp.cs3219.viz.storage.repository.PresentationAccessControlRepository;
 
 @RestController
 public class PresentationController extends BaseRestController {
@@ -27,8 +30,12 @@ public class PresentationController extends BaseRestController {
 
     private final GateKeeper gateKeeper;
 
+    private PresentationAccessControlRepository presentationAccessControlRepository;
+
     public PresentationController(PresentationLogic presentationLogic,
-                                  GateKeeper gateKeeper) {
+                                  GateKeeper gateKeeper,
+                                  PresentationAccessControlRepository presentationAccessControlRepository) {
+        this.presentationAccessControlRepository = presentationAccessControlRepository;
         this.presentationLogic = presentationLogic;
         this.gateKeeper = gateKeeper;
     }
@@ -66,10 +73,9 @@ public class PresentationController extends BaseRestController {
 
         Presentation oldPresentation = presentationLogic.findById(id)
                 .orElseThrow(() -> new PresentationNotFoundException(id));
-        gateKeeper.verifyAccessForPresentation(oldPresentation, AccessLevel.CAN_READ);
+        gateKeeper.verifyAccessForPresentation(oldPresentation, AccessLevel.CAN_WRITE);
 
         Presentation updatedPresentation = presentationLogic.updatePresentation(oldPresentation, newPresentation);
-
         return ResponseEntity
                 .created(new URI("/presentations/" + newPresentation.getId()))
                 .body(updatedPresentation);
@@ -86,4 +92,42 @@ public class PresentationController extends BaseRestController {
         return ResponseEntity.noContent().build();
     }
 
+    @PostMapping("/sharePresentation/{id}")
+    public ResponseEntity<?> sharePresentation(@RequestBody Presentation presentation, @PathVariable Long id) throws URISyntaxException {
+        Presentation oldPresentation = presentationLogic.findById(id)
+                .orElseThrow(() -> new PresentationNotFoundException(id));
+        gateKeeper.verifyAccessForPresentation(oldPresentation, AccessLevel.CAN_WRITE);
+
+        updateAccessControl(presentation);
+        Presentation updatedPresentation = presentationLogic.updatePresentation(oldPresentation, presentation);
+        return ResponseEntity
+                .created(new URI("/presentations/" + presentation.getId()))
+                .body(updatedPresentation);
+    }
+
+    /**
+     * Updates the access control of the target presentation file.
+     */
+    public void updateAccessControl(Presentation presentation) {
+        for (AccessControlMapping mapping : presentation.getMappingList()) {
+            PresentationAccessControl accessControl = new PresentationAccessControl();
+            accessControl.setUserIdentifier(mapping.getEmail());
+            accessControl.setPresentation(presentation);
+            accessControl.setAccessLevel(getAccessLevel(mapping.getAccessLevel()));
+            presentationAccessControlRepository.save(accessControl);
+        }
+    }
+
+    /**
+     * Returns the actual access level of a string equivalent.
+     */
+    public AccessLevel getAccessLevel(String accessLevel) {
+        switch (accessLevel) {
+            case "AccessLevel.CAN_WRITE":
+                return AccessLevel.CAN_WRITE;
+            case "AccessLevel.CAN_READ":
+                return AccessLevel.CAN_READ;
+        }
+        return AccessLevel.CAN_READ;
+    }
 }
