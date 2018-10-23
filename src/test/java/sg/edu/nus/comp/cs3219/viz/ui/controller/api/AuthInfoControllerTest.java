0b0c276c7f86a8b73d04ee262a0132f0d2a8a0c2
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-10-23 17:31:30 +0800

    remove all admin related code

--- src/test/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/AuthInfoControllerTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/AuthInfoControllerTest.java
@@ -52,21 +52,6 @@ public class AuthInfoControllerTest extends BaseTestREST {
                 .andExpect(jsonPath("$.loginUrl").doesNotExist())
                 .andExpect(jsonPath("$.userInfo.userEmail").value("test@example.com"))
                 .andExpect(jsonPath("$.userInfo.userNickname").value("test@example.com"))
-                .andExpect(jsonPath("$.userInfo.isAdmin").value(false));
     }
 
-    @Test
-    public void testGetAuthInfo_typicalAdminUser_shouldReturnCorrectResult() throws Exception {
-        gaeSimulation.loginAsAdmin("test2@example.com");
-
-        mvc.perform(get("/api/auth"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.isLogin").value(true))
-                .andExpect(jsonPath("$.logoutUrl")
-                        .value("http://localhost/_ah/logout?continue=http%3A%2F%2Flocalhost"))
-                .andExpect(jsonPath("$.loginUrl").doesNotExist())
-                .andExpect(jsonPath("$.userInfo.userEmail").value("test2@example.com"))
-                .andExpect(jsonPath("$.userInfo.userNickname").value("test2@example.com"))
-                .andExpect(jsonPath("$.userInfo.isAdmin").value(true));
-    }
 }
