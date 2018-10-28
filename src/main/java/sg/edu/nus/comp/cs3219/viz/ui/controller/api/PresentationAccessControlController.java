5497617b590bf3799cfb2aec613c5fc15d5925dd
tanhengyeow <E0032242@u.nus.edu>
2018-10-28 16:27:09 +0800

    The following points are updated: 1. Return access control instead of the whole presentation 2. Use valueOf() to get enum value

--- src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/PresentationAccessControlController.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/PresentationAccessControlController.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.comp.cs3219.viz.ui.controller.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,73 +34,62 @@ public class PresentationAccessControlController extends BaseRestController {
     }
 
     @PutMapping("/presentations/{id}/accessControl/{email}/{accessLevel}")
-    public ResponseEntity<?> sharePresentation(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email, @PathVariable String accessLevel) throws URISyntaxException {
+    public ResponseEntity<?> addPermission(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email, @PathVariable String accessLevel) throws URISyntaxException {
         Presentation oldPresentation = presentationLogic.findById(id)
                 .orElseThrow(() -> new PresentationNotFoundException(id));
         gateKeeper.verifyAccessForPresentation(oldPresentation, AccessLevel.CAN_WRITE);
 
-        updateAccessControl(presentation, email, accessLevel);
-        Presentation updatedPresentation = presentationLogic.updatePresentation(oldPresentation, presentation);
+        PresentationAccessControl newAccessControl = updateAccessControl(presentation, email, accessLevel);
         return ResponseEntity
-                .created(new URI("/presentations/" + presentation.getId()))
-                .body(updatedPresentation);
+                .created(new URI("/presentations/" + presentation.getId() + "/newPermission/" + newAccessControl.getId()))
+                .body(newAccessControl);
     }
 
     @PutMapping("/presentations/{id}/newAccessControl/{email}/{accessLevel}")
-    public ResponseEntity<?> updatePermissions(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email, @PathVariable String accessLevel) throws URISyntaxException {
-        Presentation oldPresentation = presentationLogic.findById(id)
-                .orElseThrow(() -> new PresentationNotFoundException(id));
+    public ResponseEntity<?> updatePermission(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email, @PathVariable String accessLevel) throws URISyntaxException {
+        presentationLogic.findById(id)
+            .orElseThrow(() -> new PresentationNotFoundException(id));
 
+        PresentationAccessControl updatedAccessControl = new PresentationAccessControl();
         for (PresentationAccessControl accessControl : presentation.getAccessControlList()) {
             if (accessControl.getUserIdentifier().equals(email) && !accessControl.getAccessLevel().equals(accessLevel)) {
                 presentationAccessControlRepository.delete(accessControl);
-                updateAccessControl(presentation, email, accessLevel);
+                updatedAccessControl = updateAccessControl(presentation, email, accessLevel);
             }
         }
 
         return ResponseEntity
-                .created(new URI("/presentations/" + presentation.getId()))
-                .body(oldPresentation);
+                .created(new URI("/presentations/" + presentation.getId() + "/updatedPermission/" + updatedAccessControl.getId()))
+                .body(updatedAccessControl);
     }
 
     @PutMapping("/presentations/{id}/{email}")
-    public ResponseEntity<?> removePermissions(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email) throws URISyntaxException {
-        Presentation oldPresentation = presentationLogic.findById(id)
-                .orElseThrow(() -> new PresentationNotFoundException(id));
+    public ResponseEntity<?> removePermission(@RequestBody Presentation presentation, @PathVariable Long id, @PathVariable String email) throws URISyntaxException {
+        presentationLogic.findById(id)
+            .orElseThrow(() -> new PresentationNotFoundException(id));
 
+        PresentationAccessControl removedAccessControl = new PresentationAccessControl();
         for (PresentationAccessControl accessControl : presentation.getAccessControlList()) {
             if (accessControl.getUserIdentifier().equals(email)) {
+                removedAccessControl = accessControl;
                 presentationAccessControlRepository.delete(accessControl);
             }
         }
 
-        Presentation updatedPresentation = presentationLogic.updatePresentation(oldPresentation, presentation);
         return ResponseEntity
-                .created(new URI("/presentations/" + oldPresentation.getId()))
-                .body(updatedPresentation);
+                .created(new URI("/presentations/" + presentation.getId() + "/removedPermission/" + removedAccessControl.getId()))
+                .body(removedAccessControl);
     }
 
     /**
      * Updates the access control of the target presentation file.
      */
-    public void updateAccessControl(Presentation presentation, String email, String accessLevel) {
+    public PresentationAccessControl updateAccessControl(Presentation presentation, String email, String accessLevel) {
         PresentationAccessControl accessControl = new PresentationAccessControl();
         accessControl.setUserIdentifier(email);
         accessControl.setPresentation(presentation);
-        accessControl.setAccessLevel(getAccessLevel(accessLevel));
+        accessControl.setAccessLevel(AccessLevel.valueOf(accessLevel));
         presentationAccessControlRepository.save(accessControl);
-    }
-
-    /**
-     * Returns the actual access level of a string equivalent.
-     */
-    public AccessLevel getAccessLevel(String accessLevel) {
-        switch (accessLevel) {
-            case "CAN_WRITE":
-                return AccessLevel.CAN_WRITE;
-            case "CAN_READ":
-                return AccessLevel.CAN_READ;
-        }
-        return AccessLevel.CAN_READ;
+        return accessControl;
     }
 }
